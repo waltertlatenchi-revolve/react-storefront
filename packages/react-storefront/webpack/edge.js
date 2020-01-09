9e880edd35f7909a9a545478a051e501cf412808
Mark Brocato <mark.brocato@moovweb.com>
2020-01-09 23:17:30 +0200

    Update error message and readme

--- packages/react-storefront/webpack/edge.js
+++ packages/react-storefront/webpack/edge.js
@@ -2,7 +2,7 @@ console.error(
   `\n` +
     `Build Failed: react-storefront/webpack/edge has been removed.  Use react-storefront-edge/webpack instead.\n` +
     `To add react-storefront-edge to your app run:\n\n` +
-    `npm i --save-dev react-storefront-edge\n\n` +
+    `npm i --save-dev react-storefront-edge@^1.0.0\n\n` +
     `Then replace react-storefront/webpack/edge with react-storefront-edge/webpack in config/webpack.prod.edge.js.\n`
 )
 
